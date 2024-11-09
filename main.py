58f2e3e72c6f8129400aed09f60079cf11ae646e
anthonella <alessandrafrutoslara@gmail.com>
2024-11-09 12:56:12 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+import string
+
+def generate_random_code(length=8):
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choice(characters) for _ in range(length))
+
+# Generate a random code of 8 characters
+random_code = generate_random_code()
+print("Your random code:", random_code)
